574462e2c2d7e703ac313b48b8cbad64f33062fe
challe002 <e195748@ie.u-ryukyu.ac.jp>
2020-08-19 15:01:05 +0900

    commit !

--- すごろくproject/Assets/Scripts/TestSelectStage.cs
+++ すごろくproject/Assets/Scripts/TestSelectStage.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TestSelectStage : MonoBehaviour
 {
     [SerializeField] Button button;
     [SerializeField] Text data;
+    static string player_data = "1";
+    static string stage_data = "Stage1";
 
     // Start is called before the first frame update
     void Start()
@@ -25,61 +25,61 @@ public class TestSelectStage : MonoBehaviour
     {
         if (button.tag.Equals("PlayerBotton"))
         {
-            PlayerBotton();
+            player_data = PlayerBotton();
         }
         else if (button.tag.Equals("StageBotton"))
         {
-            StageBotton();
+            stage_data = StageBotton();
         }
         else if (button.tag.Equals("DecisionBotton"))
         {
-            Decision();
+            Decision(stage_data);
         }
     }
-    public void PlayerBotton()
+    public string PlayerBotton()
     {
         switch (data.text)
         {
             case "1":
                 data.text = "2";
-                break;
+                return "2";
             case "2":
                 data.text = "3";
-                break;
+                return "3";
             case "3":
                 data.text = "4";
-                break;
+                return "4";
             case "4":
                 data.text = "1";
-                break;
+                return "1";
             default:
                 break;
         }
+        return null;
     }
-    public void StageBotton()
+    public string StageBotton()
     {
         switch (data.text)
         {
             case "Stage1":
                 data.text = "Stage2";
-                break;
+                return "Stage2";
             case "Stage2":
                 data.text = "Stage3";
-                break;
+                return "Stage3";
             case "Stage3":
                 data.text = "Stage4";
-                break;
+                return "Stage4";
             case "Stage4":
                 data.text = "Stage1";
-                break;
+                return "Stage1";
             default:
                 break;
-
         }
+        return null;
     }
-    public void Decision()
+    public void Decision(string stage_data)
     {
-        
-        Debug.Log("test");
+        SceneManager.LoadScene(stage_data, LoadSceneMode.Single);
     }
 }
