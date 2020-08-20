f42f27830b3225b17c289a7bc5032aa857ae6ae0
Issei <e195755@ie.u-ryukyu.ac.jp>
2020-08-20 15:10:32 +0900

    add unit test

---
+++ すごろくproject/Assets/Tests/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+namespace Tests
+{
+    public class UnitTest
+    {
+        // Testアトリビュートを付ける
+        [Test]
+        public void ExampleTest()
+        {
+            // 条件式がtrueだったら成功
+            Assert.That(1 < 10);
+        }
+    }
+}
