141b6066f64bc07e93eaa1de6a49768a9aafd137
Issei <e195755@ie.u-ryukyu.ac.jp>
2020-08-20 22:34:04 +0900

    add ViewDice

---
+++ すごろくproject/Assets/Scripts/ViewDice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewDice : MonoBehaviour
+{
+    private Animator _animator;
+    
+    public ViewDice()
+    { 
+        _animator = this.GetComponent<Animator>();
+    }
+
+    public void StartAnimation()
+    {
+        throw new NotImplementedException("実装してないで");
+    }
+
+    public void StopAnimation(int value)
+    {
+        throw new NotImplementedException("実装してないで");
+    }
+}
