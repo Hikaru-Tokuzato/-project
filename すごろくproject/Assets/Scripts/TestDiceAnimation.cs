447a2c73684e40f264a16bfc6bd3555f80946e24
challe002 <e195748@ie.u-ryukyu.ac.jp>
2020-08-23 14:44:29 +0900

    add animation

---
+++ すごろくproject/Assets/Scripts/TestDiceAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestDiceAnimation : MonoBehaviour
+{
+    [SerializeField] Button button;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(this.OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick(){
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("ButtonTrigger");
+    }
+}
