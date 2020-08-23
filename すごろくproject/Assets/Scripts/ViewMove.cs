8f2f14d908dc11344dd1541c70ebc9990a92ebdd
naoru <e195734@ie.u-ryukyu.ac.jp>
2020-08-23 15:35:04 +0900

    commit!

---
+++ すごろくproject/Assets/Scripts/ViewMove.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartAnimation()
+    {
+        throw new NotImplementedException("実装してないで");
+    }
+
+    public void StopAnimation(int value)
+    {
+        throw new NotImplementedException("実装してないで");
+    }
+}
