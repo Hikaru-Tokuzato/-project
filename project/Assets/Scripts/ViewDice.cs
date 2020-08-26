4760a38223a37b115409f2cd7ea9ec40e76901cf
Issei <e195755@ie.u-ryukyu.ac.jp>
2020-08-27 00:07:37 +0900

    add MotionChange

--- project/Assets/Scripts/ViewDice.cs
+++ project/Assets/Scripts/ViewDice.cs
@@ -6,19 +6,42 @@ using UnityEngine;
 public class ViewDice : MonoBehaviour
 {
     private Animator _animator;
-    
+    private bool m_isMotion = false;
+    public bool IsMotion{
+        get{ return m_isMotion; }
+    }
+
     public ViewDice()
     { 
         _animator = this.GetComponent<Animator>();
     }
 
-    public void StartAnimation()
+    public void StartAnimation(int value)
     {
-        throw new NotImplementedException("実装してないで");
+        _animator.SetTrigger("ButtonTrigger");
+        StartCoroutine("MotionChange");
     }
 
-    public void StopAnimation(int value)
+    IEnumerator MotionChange()
     {
-        throw new NotImplementedException("実装してないで");
+        // https://kasatanet.hatenablog.com/entry/2017/11/26/224555
+        m_isMotion = true;
+      
+        //遷移を待つ
+        float transitionTime = 0;
+        while (_animator.IsInTransition(0))
+        {
+            transitionTime += Time.deltaTime;
+            yield return 0;
+        }
+        //念のためアニメーションのステートをUpdate
+        //無いと遷移前ステートを取得しちゃうことがあります。。。
+        _animator.Update(0);
+
+        //モーション時間分だけ待つ
+        AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+        yield return new WaitForSeconds(stateInfo.length - transitionTime);
+        
+        m_isMotion = false;
     }
 }
